bb68afad7177dd68c6a85edb7eafd0d215d0b6a9
Suvash Dolai <129322686+cyber-suvash@users.noreply.github.com>
2024-10-05 14:03:15 +0530

    Update func.js

--- func.js
+++ func.js
@@ -2,25 +2,47 @@ const inputbox=document.getElementById("input-box")
 
 const idList=document.getElementById("list-container")
 
-
 const buttonid= document.querySelector('button').addEventListener('click',adTast)
 
 
-function adTast(){
-    if(inputbox.value===''){
-        alert("please add something you need to do!!")
+let count = 0; // Initialize counter
+// Function to update counter from current list items
+function updateCount() {
+    count = idList.getElementsByTagName('li').length;
+    console.log('Current count:', count);
+}
+// Initialize count on page load
+updateCount();
+
+function addTask() {
+    if (inputbox.value === '') {
+        alert("Please add something you need to do!");
+        return;
     }
-    else{
+    
+    // Changed condition from count > 9 to count >= 9
+    else if (count >= 8) {
+        alert("You've reached the maximum limit of 9 tasks!");
+        inputbox.value = "";
+        return;
+    }
+    
+    createList();
+    inputbox.value = "";
+    storeDATA();
+}
+
+
+function createList(){
+   
         let li =document.createElement('li')
         li.innerHTML=inputbox.value
         idList.appendChild(li)
-         let span =document.createElement('span')
+    
+      let span =document.createElement('span')
         span.innerHTML="\u00d7"
         li.appendChild(span)
-      
-    }
-    inputbox.value=""
-    storeDATA( )
+       count++
 }
 
 
@@ -32,6 +54,7 @@ idList.addEventListener('click',function(ov){
 
     else if(ov.target.tagName==="SPAN"){
         ov.target.parentElement.remove()
+        count--
         storeDATA()
     }
 })
